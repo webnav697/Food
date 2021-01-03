c4b7680b4196a665974093c989478faab06d30ee
webnav697 <webnav697@gmail.com>
2021-01-03 11:45:16 +0200

    Доработал модалку, добавлен фуекционал появления окна при прокрутке до низа страницы, а также добавленна возможность назначать таймаут времени после которого хотим показывать окно

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,44 @@
 "use strict";
-// popup окно
+// модальное окно
 window.addEventListener('DOMContentLoaded',()=>{
 	const modalTrigger = document.querySelectorAll('[data-modal]');// Достаём все кнопки по атрибуту date-modal
 	const modal = document.querySelector('.modal');//Достаём весь блок с формой
 	const modalCloseBtn = document.querySelector('[data-close]');// Достаём кнопку закрытия
 
-	modalTrigger.forEach(btn =>{// Циклом достаём все кнопки
-		btn.addEventListener('click', () => {// Вешаем обработчик
-		modal.classList.add('show');// Добавляем класс 
-		modal.classList.remove('hide');// Удаляем класс
+	// Функция открытия модального окна
+	function openModal() {
+		modal.classList.add('show'); // Добавляем класс 
+		modal.classList.remove('hide'); // Удаляем класс
 		document.body.style.overflow = "hidden";// Убираем прокрутку во время попапа
-		});
+		/*clearInterval(modalTimerId);*/
+		// Очищаем чтобы если пользователь уже успел сам посомтреть модалку
+		// И оно не раздражало пользователя лишний раз
+	} 
+	/*const modalTimerId = setTimeout(openModal,5000);*/
+	// Откроем окно после назначенного времени,
+	//если нужен такой функционал расскомментировать const modalTimerId 
+	// и также в функции openModal расскоментировать clearInterval(modalTimerId)
+
+	
+	// Далее Фукнция показывания модалки при долистывания до конца страницы
+	// Если нужен такой функционал,расскоментировать весь блок нужно
+	/*
+function showModalByScroll() {
+	if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+			openModal();// Показываем модалку
+			window.removeEventListener('scroll', showModalByScroll);// УДАЛЯЮ СОБЫТИЕ ПОСЛЕ ИСПОЛНЕНИЯ 1 РАЗА
+			}	
+}
+window.addEventListener ('scroll', showModalByScroll);
+*/
+
+	modalTrigger.forEach(btn =>{ // Циклом достаём все кнопки
+		btn.addEventListener('click', openModal);// Вешаем функцию на клик
 	});
 
 	function closeModal() {
-		modal.classList.remove('show');// Удаляем класс
-		modal.classList.add('hide');// Добавляем класс
+		modal.classList.remove('show'); // Удаляем класс
+		modal.classList.add('hide'); // Добавляем класс
 		document.body.style.overflow = "";
 	}
 
@@ -24,8 +47,8 @@ window.addEventListener('DOMContentLoaded',()=>{
 	// Поэтому пишем без скобок функцию
 
 	modal.addEventListener('click', (e) => {
-		if(e.target === modal){// Если клик действительно был в поле мимо крестика
-			closeModal();//Вызываем функцию закрытия
+		if(e.target === modal){ // Если клик действительно был в поле мимо крестика
+			closeModal(); // Всё равно вызываем функцию закрытия
 		}
 	});
 
